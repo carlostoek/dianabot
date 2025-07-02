3a6f1132c94132698316e71d78212dcbb8866548
carlostoek <carlostoek@gmail.com>
2025-07-02 14:03:39 -0600

    Implement phase 2 features

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from aiogram import Bot, Dispatcher
 
 from config import settings
 from database_init import init_db
-from handlers import onboarding, backpack
+from handlers import onboarding, backpack, combination, vip_access
+from middlewares.vip_middleware import VIPMiddleware
 
 
 def setup_bot() -> Dispatcher:
@@ -11,6 +12,9 @@ def setup_bot() -> Dispatcher:
     dp = Dispatcher()
     dp.include_router(onboarding.router)
     dp.include_router(backpack.router)
+    dp.include_router(combination.router)
+    dp.include_router(vip_access.router)
+    dp.message.middleware(VIPMiddleware(channel_id=0))
     return bot, dp
 
 async def main():
